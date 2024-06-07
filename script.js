be225f000efb726f55d17355a7ac069d90cf812a
MrVladikk <98462351+MrVladikk@users.noreply.github.com>
2024-06-07 10:27:26 +0300

    дз 10 темная тема

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-//Задание 1 которое 2
-function factorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-}
-console.log(factorial(7));
-console.log(factorial(0));
-console.log(factorial(1));
-console.log(factorial(8));
-//Задание 2 которое 3
-function geolocation() {
-  let intervalID = setInterval(function () {
-    let result = confirm("Включите геолокацию");
-    if (result) {
-      clearInterval(intervalID);
+document.addEventListener("DOMContentLoaded", () => {
+  let toggleButton = document.getElementById("theme-toggle");
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+      toggleButton.textContent = "Switch to Light Mode";
+    } else {
+      toggleButton.textContent = "Switch to Dark Mode";
     }
-  }, 1000);
-}
-geolocation();
-//Второй способ
-function geolocation2() {
-  function ask() {
-    let result = confirm("Включите геолокацию");
-    if (result) {
-      return;
-    }
-    setTimeout(ask, 1000);
-  }
-  ask();
-}
-geolocation2();
+  });
+});
