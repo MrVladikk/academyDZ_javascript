c18b500fd72e4b5bd885a8fb13c946719cd9ac6f
MrVladikk <98462351+MrVladikk@users.noreply.github.com>
2024-07-17 21:46:44 +0300

    питон второе задание задачи

---
+++ classwork2.py
@@ -0,0 +1,86 @@
+# A = int(input("введите число "))
+# if A >= 0:
+#     print(A + 1)
+# else:
+#     print(A)
+
+
+# speed = 80
+# if speed >= 130 :
+#     print("вы нарушили")
+# else:
+#     print("скорость в пределах нормы ")
+
+
+# A = int(input("число А "))
+# B = int(input("число В "))
+# if A == B or B == A:
+#     A = A + B
+#     B = (B + A) - B
+#     print(A,B)
+# else:
+#     A = 0
+#     B = 0
+#     print(A,B)
+
+
+
+# user_data = -4
+# if user_data == 5:
+#     print("A")
+# else:
+#     if user_data == 4:
+#         print("B")
+#     else:
+#         if user_data == 3:
+#             print("C")
+#         else:
+#             if user_data == 2:
+#                 print("D")
+#             else:
+#                 if user_data == 1:
+#                     print("E")
+#                 else:
+#                     if user_data ==0:
+#                         print("F")
+#                     else:
+#                         print("ERROR")
+
+
+# time = 6
+# ticket = 0
+# money = 0
+# luggage = 0
+# if 6 <= time <= 24 and (money or ticket):
+#     if luggage == 0:
+#         print("можно ехать")
+#     else:
+#         print("есть багаж")
+# else:
+#     print("время вышло или нет денег или нет белета ")
+
+
+
+# T = 200
+# if T < 5:
+#     print("холодно")
+# else:
+#     if T >= 5 and T < 19  :
+#         print("на улице прохладно")
+#     else:
+#         if T >= 20:
+#             print("на улице тепло")
+
+
+# m = 85
+# h = float(1.8)
+# I = m/h**2
+# if I >=18 and I < 26:
+#     print(f"начение в норме {I} ")
+# else:
+#     if I < 18:
+#         print(f"значение меньше нормы {I}")
+#     else:
+#         if I > 25:
+#             print(f"значение больше нормы {I} ")
+
